645e45e3e0ba7fd0802ee7341fc48a904b915be5
vishu512 <70514375+vishu512@users.noreply.github.com>
2025-02-06 01:25:21 +0530

    Update modal.js

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,7 +1,3 @@
-// Modal functionality
-const loginBtn = document.getElementById("login-btn");
-const modal = document.getElementById("login-modal");
-const closeButton = document.querySelector(".close-button");
 const profileBtn = document.getElementById("profile-btn");
 const profileMenu = document.querySelector(".profile-menu");
 
@@ -9,12 +5,17 @@ profileBtn.addEventListener("click", () => {
     profileMenu.style.display = profileMenu.style.display === "none" ? "block" : "none";
 });
 
-// Close the dropdown if clicked outside
+// Close dropdown if clicked outside
 window.addEventListener("click", (event) => {
     if (!event.target.closest('.profile-dropdown')) {
         profileMenu.style.display = "none";
     }
 });
+
+// Modal functionality
+const loginBtn = document.getElementById("login-btn");
+const modal = document.getElementById("login-modal");
+const closeButton = document.querySelector(".close-button");
 
 loginBtn.addEventListener("click", () => {
     modal.style.display = "block";
